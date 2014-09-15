13203abc0fdd55b6662d8b234e245ad2019671d5
qunying <qunying@kooboo.com>
2014-09-15 16:00:20 +0800

    samsung header

---
+++ assets/js/templates.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.slider').flexslider({
+        controlNav: false,
+        directionNav: false,
+        start: function(slider) {
+            //$(slider.directionNav).appendTo('.slider-topic');
+        }
+    });
+});
+
